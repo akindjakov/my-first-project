6c410ebc6dd8a1012f40e9ebab1768dde28ec201
akindjakov <aleksejs.kindjakovs@gmail.com>
2023-03-06 20:45:19 +0200

    [classroom_06032023] Added new examples and new classes

--- src/main/java/lv/acodemy/classroom/ifElse.java
+++ src/main/java/lv/acodemy/classroom/ifElse.java
@@ -64,5 +64,51 @@ public class ifElse {
         } else {
             System.out.println("Welcome to our shop!");
         }
+        // IF John = Hello John!
+        // IF Zina = You are not welcome!
+        // All others = Text
+
+        String name = "John";
+                if(name.equals("John")) {
+                    System.out.println("Hello John!");
+                } else if(name.equals("john")) {
+                    System.out.println("Hi John");
+                } else if(name.equals("Zina")) {
+                    System.out.println("You are not welcome!");
+                } else {
+                    System.out.println("Very interesting name");
+                }
+
+         //switch case
+        // ||=OR
+        // && AND
+        // switch to if -> Mac Option+Enter , PC Alt+Enter
+
+        int currentMonth = 12;
+        if (currentMonth == 12 || currentMonth == 1 || currentMonth == 2) {
+            System.out.println("This is winter month");
+        } else if (currentMonth == 3 || currentMonth == 4 || currentMonth == 5) {
+            System.out.println("This is spring month");
+        } else if (currentMonth == 6 || currentMonth == 7 || currentMonth == 8) {
+            System.out.println("This is summer month");
+        } else if (currentMonth == 9 || currentMonth == 10 || currentMonth == 11) {
+            System.out.println("This is autumn month");
+        } else {
+            throw new IllegalStateException("Unexpected value: " + currentMonth);
+        }
+
+        // if student 1 = john and student 2 max
+        // Print: they will work together
+
+        String student1 = "John";
+        String student2 = "Max";
+        if (student1.equals("John") && student2.equals("Max")) {
+            System.out.println("John and Max will work together");
+        } else if (student1.equals("john") && student2.equals("max")) {
+            System.out.println("John and Max will work together");
+        } else {
+            System.out.println("John and Max will work separately");
+        }
+
     }
 }

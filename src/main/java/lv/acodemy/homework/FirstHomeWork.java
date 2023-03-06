f8bee1b623349712eba724e07ec90561d64a1536
akindjakov <aleksejs.kindjakovs@gmail.com>
2023-03-06 15:50:15 +0200

    [homework_01032023] Branch add to the first homework

---
+++ src/main/java/lv/acodemy/homework/FirstHomeWork.java
@@ -0,0 +1,8 @@
+package lv.acodemy.homework;
+
+public class FirstHomeWork
+        // Create 2 variable for each data type (meaningful)
+        // Concateation
+        // Play with operators
+{
+}
